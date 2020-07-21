6d80855aecf9b09a0653525932bed915b2a02735
Christophe Arribat <christophe.arribat@cegedim-activ.com>
2020-07-21 09:31:03 +0200

    fin 1er jour

--- app/produitCtrl/produitCtrl.js
+++ app/produitCtrl/produitCtrl.js
@@ -1,6 +1,6 @@
 angular.module('app').controller('produitCtrl', ['$scope', function($scope) {
-    this.chaine1='Beurre';
-    this.produit={nom:'beurre salé',prix:2.00,desc:'Beurre salé ++',img:'can-we-make-butter',stars:5}
-    $scope.onaddcartclick=function(prod){console.log('produit ajouté: ',prod)}
+    this.chaine1 = 'Beurre';
+    this.produit = {nom:'beurre doux',prix:2.00,desc:'Beurre doux +++',img:'5052-1.jpg',stars:5}
+    $scope.onaddcartclick = function(prod){console.log('produit ajouté: ',prod)}
 }]);
 
--- app/produitsCtrl/produitsCtrl.js
+++ app/produitsCtrl/produitsCtrl.js
@@ -1,8 +1,8 @@
 angular.module('app').controller('produitsCtrl', ['$scope',function($scope){
     this.arrayProduits=[
-        {nom:'beurre 1er prix',prix:0.50,desc:'Beurre ripou',img:'can-we-make-butter',stars:1},
-        {nom:'beurre salé',prix:1.30,desc:'Beurre salé ++',img:'can-we-make-butter',stars:3},
-        {nom:'beurre doux',prix:2.00,desc:'Beurre doux +++',img:'can-we-make-butter',stars:5}
+        {nom:'beurre 1er prix',prix:0.50,desc:'Beurre ripou',img:'can-we-make-butter.jpg',stars:1},
+        {nom:'beurre salé',prix:1.30,desc:'Beurre salé ++',img:'580x580.webp',stars:3},
+        {nom:'beurre doux',prix:2.00,desc:'Beurre doux +++',img:'5052-1.jpg',stars:5}
     ];
-    $scope.onviewproduitclick=function(produit){console.log(produit);}
+    $scope.onviewproduitclick=function(produit){console.log(produit);} 
 }]);
